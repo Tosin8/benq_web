e4eed0a36dd70c68ca4efa723ff6b72e7550fbe4
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-04 22:28:02 +0100

    Update landing_page.dart

--- lib/landing_page/landing_page.dart
+++ lib/landing_page/landing_page.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/app_bar.dart';
-
 class Landing_Page extends StatelessWidget {
   const Landing_Page({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-              image: DecorationImage
+        body: Container(
+      height: double.infinity,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          image: DecorationImage
 // NetworkImage
-                  (
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        'assets/images/bkg.png',
-                      ))),
-          child: Column(
-            children:  const [Web_AppBar],
-          )),
-    );
+              (
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    'assets/images/bkg.png',
+                  ))),
+      //  child: Column(
+      // children:  const [Web_AppBar],
+      // )
+    ));
+    // )
   }
 }
