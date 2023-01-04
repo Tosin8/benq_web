d0d372915cdd471ec631a1c915a3deeca5832b9d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-01-04 22:25:37 +0100

    Updating

---
+++ lib/landing_page/widgets/app_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Web_AppBar extends StatefulWidget {
+  const Web_AppBar({super.key});
+
+  @override
+  State<Web_AppBar> createState() => _Web_AppBarState();
+}
+
+class _Web_AppBarState extends State<Web_AppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
